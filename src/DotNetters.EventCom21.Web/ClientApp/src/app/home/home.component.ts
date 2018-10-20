a254e793c09764eb7eceae8899b9c7a3b6074c77
Kash <castillo.sergio@gmail.com>
2018-10-20 17:22:39 +0200

    Proyecto web migrado a 2.1

---
+++ src/DotNetters.EventCom21.Web/ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import * as signalR from "@aspnet/signalr";
+declare var jquery: any;
+declare var $: any;
+
+@Component({
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent implements OnInit {
+    private _hubConnection: signalR.HubConnection;
+    public async: any;
+    tuser = '';
+    message = '';
+    messages: any[] = [];
+
+    constructor() {
+    }
+
+    public sendMessage(): void {
+        this._hubConnection.invoke('Send', this.tuser, this.message);
+        //this.messages.unshift({ 'user': this.tuser, 'message': this.message, 'sentorreceived': 'Sent' });
+    }
+
+    ngOnInit() {
+        this._hubConnection = new signalR.HubConnectionBuilder().withUrl("/messaging").build();
+
+        this._hubConnection.on('Send', (user: string, message: string) => {
+            let d = new Date();
+            let dStr = d.getDate() + '/' + d.getMonth() + 1 + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes();
+            this.messages.unshift({ 'user': user, 'message': message, 'sentorreceived': 'Received', 'date': dStr  });
+        });
+
+        this._hubConnection.start()
+            .then(() => {
+                console.log('Hub connection started')
+            })
+            .catch(err => {
+                console.log('Error while establishing connection')
+            });
+    }
+}
