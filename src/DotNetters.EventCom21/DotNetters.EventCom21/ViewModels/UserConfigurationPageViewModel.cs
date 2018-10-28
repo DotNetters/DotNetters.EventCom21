084e2bc86c556b165e2b05921ed33992971ef181
Kash <castillo.sergio@gmail.com>
2018-10-28 20:01:53 +0100

    Configuración del nombre de usuario y almacenamiento en preferencias del dispositivo

--- src/DotNetters.EventCom21/DotNetters.EventCom21/ViewModels/UserConfigurationPageViewModel.cs
+++ src/DotNetters.EventCom21/DotNetters.EventCom21/ViewModels/UserConfigurationPageViewModel.cs
@@ -4,6 +4,8 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace DotNetters.EventCom21.ViewModels
 {
@@ -11,7 +13,33 @@ namespace DotNetters.EventCom21.ViewModels
 	{
         public UserConfigurationPageViewModel(INavigationService navigationService) : base(navigationService)
         {
-
+            UserName = Preferences.Get("EventCom.UserName", string.Empty);
         }
-	}
+
+        void SetUserInPreferences()
+        {
+            Preferences.Set("EventCom.UserName", UserName);
+            MessagingCenter.Send(this, "UserConfigured", UserName);
+        }
+
+        string userName;
+        /// <summary>
+        /// Nombre del usuario
+        /// </summary>
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                SetProperty(ref userName, value);
+                if (!string.IsNullOrWhiteSpace(userName))
+                {
+                    SetUserInPreferences();
+                }
+            }
+        }
+    }
 }
